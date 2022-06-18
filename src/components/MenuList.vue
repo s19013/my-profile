f4bea7e07e1a77c667ef8e0718e67a1711846b55
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-18 11:38:20 +0900

    toのパスを変えた

--- src/components/MenuList.vue
+++ src/components/MenuList.vue
@@ -1,22 +1,26 @@
 <template>
-  <div>
+  <div class="menu">
     <router-link to="/" class="router-link">基本情報</router-link>
-    <router-link to="/MyWorks" class="router-link">作品</router-link>
+    <router-link to="/MyWorks/Original" class="router-link">作品</router-link>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'MenuList',
-
-    data: () => ({
-      
-    }),
+    name: 'MenuListView',
+    data() {
+      return {
+        
+      }
+    },
   }
 </script>
 
 <style lang="scss" scoped>
+.menu{
+  margin: 0 auto
+}
 .router-link {
-  margin:0 2vmin
+    margin:0 2vmin
 }
 </style>
