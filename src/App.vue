e1dec8db133068f25ba597728cedd11802194ab8
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-18 10:42:41 +0900

    menulist設置

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <v-container>
-    <router-view></router-view>
-  </v-container>
+  <div>
+    <v-system-bar>
+      <menu-list></menu-list>
+    </v-system-bar>
+
+    <v-container>
+      <router-view></router-view>
+    </v-container>
+  </div>
 </template>
 
 <script>
-
+import MenuList  from "./components/MenuList.vue";
 
 export default {
   name: 'App',
 
   components: {
-    
+    MenuList
   },
 
   data: () => ({
