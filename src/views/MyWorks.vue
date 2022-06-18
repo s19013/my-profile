8c9a5f2c151ccbab15dfd326678ac38771dda429
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-18 10:44:29 +0900

    新規作成

---
+++ src/views/MyWorks.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MyWorks',
+    data: () => ({
+      
+    }),
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
