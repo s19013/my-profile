35bf756a31e2ae319526ced75a4ec7dfcf5ee418
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-18 11:38:00 +0900

    新規追加

---
+++ src/views/MyWorks/Original.vue
@@ -0,0 +1,21 @@
+<template>
+  <div >
+    original
+    original
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MyWorks-OriginalView',
+    data() {
+      return {
+        
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/MyWorks/Samples.vue
@@ -0,0 +1,20 @@
+<template>
+  <div >
+    sample
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MyWorks-SamplesView',
+    data() {
+      return {
+        
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
