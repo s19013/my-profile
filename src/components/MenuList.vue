8e8788d2b18bf716de83c0961076f6a5724d9475
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-18 10:43:45 +0900

    新規作成

---
+++ src/components/MenuList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <router-link to="/" class="router-link">基本情報</router-link>
+    <router-link to="/MyWorks" class="router-link">作品</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MenuList',
+
+    data: () => ({
+      
+    }),
+  }
+</script>
+
+<style lang="scss" scoped>
+.router-link {
+  margin:0 2vmin
+}
+</style>
