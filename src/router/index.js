7e54f666172a7237fb29d67be5432f33066cc53a
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-18 11:38:34 +0900

    新規パスを追加

--- src/router/index.js
+++ src/router/index.js
@@ -12,9 +12,29 @@ const routes = [
   },
   {
     path: "/MyWorks",
+    component: () => import('../views/MyWorks/MyWorks.vue'),
+    children:[
+        {
+            path:'Original',
+            component: () => import('../views/MyWorks/Original.vue')
+        },
+        {
+            path:'Samples',
+            component: () => import('../views/MyWorks/Samples.vue')
+        }
+    ]
+  },
+  {
+    path: "/MyWorks",
     name: "MyWorks",
-    component: () => import('../views/MyWorks.vue')
-  }
+    component: () => import('../views/MyWorks/MyWorks.vue')
+  },
+  {
+    path: "/MyWorks",
+    name: "MyWorks",
+    component: () => import('../views/MyWorks/MyWorks.vue')
+  },
+
 ]
 
 const router = new VueRouter({
