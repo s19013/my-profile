1daa5b74a8b84f1fb41e4f469110ec697033f966
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-19 16:35:16 +0900

    nameComponent設置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    
+    <name-component></name-component>
   </div>
 </template>
 
 <script>
+import NameComponent from "../components/home/NameComponent.vue";
   export default {
     name: 'HomeView',
+    components:{
+      NameComponent
+    },
     data() {
       return {
         
