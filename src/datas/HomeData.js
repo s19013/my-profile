e143b214a5c10c3f2e537eac9846f9d1f6d642d6
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-19 16:35:46 +0900

    homeで使うデータを保存

---
+++ src/datas/HomeData.js
@@ -0,0 +1,10 @@
+export class HomeData{
+    constructor(){
+        this.familyName ="知念";
+        this.familyNameRubi ="ちねん";
+        this.secondName ="英弥";
+        this.secondNameRubi ="ひでや";
+
+        
+    }
+}
