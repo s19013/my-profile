4b281868a87577377c5ca8c384b77dfb6dd8c5c0
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-19 22:14:48 +0900

    学歴追加

--- src/datas/HomeData.js
+++ src/datas/HomeData.js
@@ -21,5 +21,33 @@ export class HomeData{
                 url  :'https://github.com/s19013/',
             },
         ]
+        // 職歴,学歴
+        this.careerList = [
+            {
+                year :2015,
+                month:3,
+                detail:"那覇市立小禄中学校 卒業"
+            },
+            {
+                year :2015,
+                month:4,
+                detail:"沖縄県立那覇高等学校 入学"
+            },
+            {
+                year :2018,
+                month:3,
+                detail:"沖縄県立那覇高等学校 卒業"
+            },
+            {
+                year :2019,
+                month:4,
+                detail:"専門学校ITカレッジ沖縄 ITスペシャリスト科 入学"
+            },
+            {
+                year :2022,
+                month:3,
+                detail:"専門学校ITカレッジ沖縄 ITスペシャリスト科 卒業"
+            },
+        ]
     }
 }
