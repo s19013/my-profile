8a2f8f6d933acc1ae31593bf9bfcab8f8d6bbcc6
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-19 17:19:16 +0900

    DateOfBirthComponentを設置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
-    <name-component></name-component>
+    <name-component :name="homeData.name"></name-component>
+    <date-of-birth-component :birthDay="homeData.birthDay"></date-of-birth-component>
   </div>
 </template>
 
 <script>
 import NameComponent from "../components/home/NameComponent.vue";
+import DateOfBirthComponent from "../components/home/DateOfBirthComponent.vue";
+import  {HomeData}  from "../datas/HomeData.js";
   export default {
     name: 'HomeView',
     components:{
-      NameComponent
+      NameComponent,
+      DateOfBirthComponent
     },
     data() {
       return {
-        
+        homeData:new HomeData
       }
     },
   }
