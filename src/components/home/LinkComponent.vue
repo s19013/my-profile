66e9f4896a6e9122025239e2c832b61cf197dca2
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-19 19:00:27 +0900

    仮完成

--- src/components/home/LinkComponent.vue
+++ src/components/home/LinkComponent.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
-    
+    <ul>
+      <li v-for="link of linkList" :key="link.title" >
+        <a :href="link.url">
+          <p>{{link.title}}</p>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'HomeView',
+    name: 'LinkComponent',
     data() {
       return {
         
       }
     },
+    props:['linkList']
   }
 </script>
+
+<style lang="scss" scoped>
+  li{list-style-type: none;}
+</style>
