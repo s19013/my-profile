eb36dc0ac91f59012a61838af3a165d2319f66a4
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-19 15:48:00 +0900

    追加

--- src/components/MenuList.vue
+++ src/components/MenuList.vue
@@ -2,6 +2,8 @@
   <div class="menu">
     <router-link to="/" class="router-link">基本情報</router-link>
     <router-link to="/MyWorks/Original" class="router-link">作品</router-link>
+    <router-link to="/FQA"   class="router-link">よくある質問</router-link>
+    <router-link to="/Hobby" class="router-link">趣味</router-link>
   </div>
 </template>
 
