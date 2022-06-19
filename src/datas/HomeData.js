4b4ed3dd9634a06891d61e3138a14fa21a957575
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-19 23:27:46 +0900

    資格を追加

--- src/datas/HomeData.js
+++ src/datas/HomeData.js
@@ -24,29 +24,56 @@ export class HomeData{
         // 職歴,学歴
         this.careerList = [
             {
-                year :2015,
-                month:3,
+                year  :2015,
+                month :3,
                 detail:"那覇市立小禄中学校 卒業"
             },
             {
-                year :2015,
-                month:4,
+                year  :2015,
+                month :4,
                 detail:"沖縄県立那覇高等学校 入学"
             },
             {
-                year :2018,
-                month:3,
+                year  :2018,
+                month :3,
                 detail:"沖縄県立那覇高等学校 卒業"
             },
             {
-                year :2019,
-                month:4,
+                year  :2019,
+                month :4,
                 detail:"専門学校ITカレッジ沖縄 ITスペシャリスト科 入学"
             },
             {
-                year :2022,
-                month:3,
+                year  :2022,
+                month :3,
                 detail:"専門学校ITカレッジ沖縄 ITスペシャリスト科 卒業"
+            },
+        ]
+        this.qualificationList = [
+            {
+                year  :2019,
+                month :9,
+                detail:"SEA/J Certified Security Basic Master"
+            },
+            {
+                year  :2020,
+                month :6,
+                detail:"サーティファイ javaプログラミング能力認定試験 3級"
+            },
+            {
+                year  :2020,
+                month :9,
+                detail:"CompTIA IT Fundamentals"
+            },
+            {
+                year  :2021,
+                month :8,
+                detail:"Oracle Certified Java Programmer, Bronze SE"
+            },
+            {
+                year  :2022,
+                month :2,
+                detail:"Oracle  Master Bronze 12c SQL基礎"
             },
         ]
     }
