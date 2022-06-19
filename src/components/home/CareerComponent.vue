e972f2d6888a3f6518614f002d75dabc67a0d313
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-19 22:14:02 +0900

    仮完成

--- src/components/home/CareerComponent.vue
+++ src/components/home/CareerComponent.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    
+    <table >
+      <tr class="Heading"><th>年</th> <th>月</th> <th>学歴 職歴</th></tr>
+      <tr v-for="(career,index) of careerList" :key="index">
+        <th>{{career.year}}</th>
+        <th>{{career.month}}</th>
+        <th>{{career.detail}}</th>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -12,5 +19,23 @@
         
       }
     },
+    props:['careerList']
   }
 </script>
+
+<style lang="scss" scoped>
+  table {
+    width: 100%;
+    border-spacing: 0;
+    .Heading{
+      background-color: #3f72af;
+      color: #f9f7f7;
+    }
+    th, td {
+      border: 1px solid #4f4d47;
+      text-align: center;
+      padding: 2vmin;
+    }
+  }
+
+</style>
