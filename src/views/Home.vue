1704a5ba4aa436403acfff07e72ce84c44a3633b
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-19 22:14:20 +0900

    propsを渡した

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,21 +3,28 @@
     <name-component :name="homeData.name"></name-component>
     <date-of-birth-component :birthDay="homeData.birthDay"></date-of-birth-component>
     <link-component :linkList="homeData.linkList"> </link-component>
+    <career-component :careerList="homeData.careerList"></career-component>
     
   </div>
 </template>
 
 <script>
+// コンポーネント
 import NameComponent from "../components/home/NameComponent.vue";
 import DateOfBirthComponent from "../components/home/DateOfBirthComponent.vue";
 import LinkComponent from "../components/home/LinkComponent.vue";
+import CareerComponent from "../components/home/CareerComponent.vue";
+
+
+//データ
 import  {HomeData}  from "../datas/HomeData.js";
   export default {
     name: 'HomeView',
     components:{
       NameComponent,
       DateOfBirthComponent,
-      LinkComponent
+      LinkComponent,
+      CareerComponent
     },
     data() {
       return {
@@ -26,3 +33,5 @@ import  {HomeData}  from "../datas/HomeData.js";
     },
   }
 </script>
+
+
