ed1704749df11422ff5d8a3fca99291dac2ceb71
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-19 19:00:52 +0900

    link-component設置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,22 @@
   <div>
     <name-component :name="homeData.name"></name-component>
     <date-of-birth-component :birthDay="homeData.birthDay"></date-of-birth-component>
+    <link-component :linkList="homeData.linkList"> </link-component>
+    
   </div>
 </template>
 
 <script>
 import NameComponent from "../components/home/NameComponent.vue";
 import DateOfBirthComponent from "../components/home/DateOfBirthComponent.vue";
+import LinkComponent from "../components/home/LinkComponent.vue";
 import  {HomeData}  from "../datas/HomeData.js";
   export default {
     name: 'HomeView',
     components:{
       NameComponent,
-      DateOfBirthComponent
+      DateOfBirthComponent,
+      LinkComponent
     },
     data() {
       return {
