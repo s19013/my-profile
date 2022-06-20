1aa849f90b3606dd7f1f5a4313f59ab65a65d732
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-21 00:32:55 +0900

    文章追加

--- src/views/MyWorks/Original.vue
+++ src/views/MyWorks/Original.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="cardGroup">
-    <div v-for="(element,index) of originalWorks.works" :key="index">
-
-      <div >
-        <work-card-component :work = element></work-card-component>
-      </div>
-
+  <div>
+    <div class="heading">
+      <p>設計などから自分で考えた作品</p>
     </div>
+      <div class="cardGroup">
+        <div v-for="(element,index) of originalWorks.works" :key="index">
+
+          <div >
+            <work-card-component :work = element></work-card-component>
+          </div>
+
+        </div>
+      </div>
   </div>
 </template>
 
@@ -33,6 +38,14 @@ import { MyWorksOriginalWorks } from "@/datas/MyWorksOriginalWorks.js";
     display: flex;
     flex-wrap:wrap;
     
+  }
+}
+.heading{
+  border: 1px solid black;
+  margin: 2vh 0;
+  background-color: #dbe2ef;
+  p{
+    margin: 0 2vw;
   }
 }
 </style>
