2ff7f3ca7abd3f5327930f77fb68696b2ce7e21a
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-21 00:26:05 +0900

    データ追加,readmeUrl追加

--- src/datas/MyWorksOriginalWorks.js
+++ src/datas/MyWorksOriginalWorks.js
@@ -12,46 +12,37 @@ export class MyWorksOriginalWorks{
         this.works = [
             {
                 imgUrl:null,
-                title:"Knowledge Sharing Board",
+                title :"Knowledge Sharing Board",
+                readmeUrl:"https://github.com/s19013/knowledge-sharing-board",
                 codeUrl:"https://github.com/s19013/knowledge-sharing-board",
                 workUrl:"http://knowledge-sharing-board.link/",
                 comment:"laravelの練習として作りました",
             },
             {
+                imgUrl:"originalWorks/gobblet.png",
+                title :"ゴブレット",
+                readmeUrl:"https://github.com/s19013/GraduationTasks",
+                codeUrl:"https://github.com/s19013/GraduationTasks/tree/main/project/Gobblet5/app/src/main",
+                workUrl:"https://play.google.com/store/apps/details?id=com.game.gobblet5&hl=ja&gl=US",
+                comment:"コンピューターのアルゴリズムを作りました",
+            },
+            {
                 imgUrl:null,
-                title:"",
-                codeUrl:null,
-                workUrl:null,
+                title :"ブラックジャック",
+                readmeUrl:"https://github.com/s19013/web-blackjack-vue",
+                codeUrl:"https://github.com/s19013/web-blackjack-vue/src",
+                workUrl:"https://s19013.github.io/web-blackjack-vue/",
                 comment:null,
             },
             {
                 imgUrl:null,
-                title:"",
-                codeUrl:null,
-                workUrl:null,
-                comment:null,
+                title :"自己紹介サイト",
+                readmeUrl:"https://github.com/s19013/my-profile",
+                codeUrl:"https://github.com/s19013/my-profile",
+                workUrl:"https://s19013.github.io/web-blackjack-vue/src",
+                comment:"このサイトです",
             },
-            {
-                imgUrl:null,
-                title:"",
-                codeUrl:null,
-                workUrl:null,
-                comment:null,
-            },
-            {
-                imgUrl:null,
-                title:"",
-                codeUrl:null,
-                workUrl:null,
-                comment:null,
-            },
-            {
-                imgUrl:null,
-                title:"",
-                codeUrl:null,
-                workUrl:null,
-                comment:null,
-            },
+
         ]
     }
 }
