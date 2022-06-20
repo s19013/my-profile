7c87d504eb676494dfa8967e59cf492dafac1ec5
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-20 18:43:32 +0900

    新しいデータリストを作った

---
+++ src/datas/MyWorksOriginalWorks.js
@@ -0,0 +1,57 @@
+// class baseData{
+//     constructor(imgUrl,title,codeUrl,workUrl,comment) {
+//         this.imgUrl = imgUrl
+//         this.title = title
+//         this.codeUrl = codeUrl
+//         this.workUrl = workUrl
+//         this.comment = comment
+//     }
+// }
+export class MyWorksOriginalWorks{
+    constructor() {
+        this.works = [
+            {
+                imgUrl:null,
+                title:"Knowledge Sharing Board",
+                codeUrl:"https://github.com/s19013/knowledge-sharing-board",
+                workUrl:"http://knowledge-sharing-board.link/",
+                comment:"laravelの練習として作りました",
+            },
+            {
+                imgUrl:null,
+                title:"",
+                codeUrl:null,
+                workUrl:null,
+                comment:null,
+            },
+            {
+                imgUrl:null,
+                title:"",
+                codeUrl:null,
+                workUrl:null,
+                comment:null,
+            },
+            {
+                imgUrl:null,
+                title:"",
+                codeUrl:null,
+                workUrl:null,
+                comment:null,
+            },
+            {
+                imgUrl:null,
+                title:"",
+                codeUrl:null,
+                workUrl:null,
+                comment:null,
+            },
+            {
+                imgUrl:null,
+                title:"",
+                codeUrl:null,
+                workUrl:null,
+                comment:null,
+            },
+        ]
+    }
+}
