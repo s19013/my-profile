985d144b5952f6db5cf0401e97e40f37ffc937d9
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-20 12:13:10 +0900

    メニューリストをコンポーネント化

---
+++ src/components/MenuListComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="menu">
+    <div v-for="(element,index) of list" :key="index">
+      <router-link :to="element.to"
+      class="router-link"
+      :style="size"
+      >
+        {{element.title}}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MenuListComponent',
+    data() {
+      return {
+        
+      }
+    },
+    props:['list','fontSize'],
+    computed:{
+      size(){
+        return {'--font-size':this.fontSize}
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.menu{
+  margin: 0 auto;
+  display: flex;
+}
+.router-link {
+    margin:0 2vmin;
+    font-size:var(--font-size);
+}
+</style>
