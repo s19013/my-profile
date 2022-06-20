f2591a88ec5f8924b87d87f6d912a347877440d6
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-20 18:44:39 +0900

    データをforでまとめて表示

--- src/views/MyWorks/Original.vue
+++ src/views/MyWorks/Original.vue
@@ -1,21 +1,38 @@
 <template>
-  <div >
-    original
-    original
+  <div class="cardGroup">
+    <div v-for="(element,index) of originalWorks.works" :key="index">
+
+      <div >
+        <work-card-component :work = element></work-card-component>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
+import WorkCardComponent from "@/components/WorkCardComponent.vue";
+import { MyWorksOriginalWorks } from "@/datas/MyWorksOriginalWorks.js";
+
   export default {
     name: 'MyWorks-OriginalView',
+    components:{
+      WorkCardComponent
+    },
     data() {
       return {
-        
+        originalWorks:new MyWorksOriginalWorks,
       }
     },
   }
 </script>
 
 <style lang="scss" scoped>
-
+@media (min-width: 501px){
+  .cardGroup{
+    display: flex;
+    flex-wrap:wrap;
+    
+  }
+}
 </style>
