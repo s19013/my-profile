cd251c476b46727354319e6e4f6880c6f869d09e
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-21 11:49:56 +0900

    学歴､資格は折りたたみにした

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,57 @@
 <template>
   <div>
-    <p class="lastUpdated">2022/6/19</p>
-    <name-component :name="homeData.name"></name-component>
-    <date-of-birth-component :birthDay="homeData.birthDay"></date-of-birth-component>
-    <link-component :linkList="homeData.linkList"> </link-component>
+    <p class="lastUpdated"> 最終更新日:2022/6/19</p>
+    <name-component
+    :name="homeData.name"
+    class="name-component">
+    </name-component>
+
+    <date-of-birth-component
+    :birthDay="homeData.birthDay"
+    class="date-of-birth-component">
+    </date-of-birth-component>
+
+    <link-component :linkList="homeData.linkList">
+    </link-component>
+
     <!-- 学歴､職歴 -->
-    <history-component :list="homeData.careerList" detailName="学歴 職歴"></history-component>
+    <div class="career">
+      <div @click="showCareerList = !showCareerList">
+        学歴､職歴
+        <v-btn icon>
+          <v-icon>{{ showCareerList ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+        </v-btn>
+      </div>
+
+      <v-expand-transition>
+        <div v-show="showCareerList">
+          <history-component :list="homeData.careerList" detailName="学歴 職歴">
+          </history-component>
+        </div>
+      </v-expand-transition>
+    </div>
+    <!--  -->
+
     <!-- 資格名 -->
-    <history-component :list="homeData.qualificationList" detailName="資格名"></history-component>
-    
+    <div>
+      <div @click="showQualificationList = !showQualificationList">
+        <p>
+          資格
+          <v-btn icon>
+            <v-icon>{{ showQualificationList ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+          </v-btn>
+        </p>
+      </div>
+
+      <v-expand-transition>
+        <div v-show="showQualificationList">
+          <history-component
+            :list="homeData.qualificationList" detailName="資格名">
+          </history-component>
+        </div>
+      </v-expand-transition>
+    </div>
+    <!--  -->
   </div>
 </template>
 
@@ -33,13 +76,19 @@ import  {HomeData}  from "../datas/HomeData.js";
     },
     data() {
       return {
-        homeData:new HomeData
+        homeData:new HomeData,
+        // トリガー
+        showCareerList:false,
+        showQualificationList:false
       }
     },
   }
 </script>
 
 <style lang="scss">
-  .lastUpdated{font-size:2vmin ;}
+  .lastUpdated{
+    font-size:0.5vw ;
+    text-align: right
+    }
 </style>
 
