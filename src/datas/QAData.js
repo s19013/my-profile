2b08e885cb9f2b5074743056ff5ad8b4e639f027
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-21 12:35:24 +0900

    データの雛形を作った

---
+++ src/datas/QAData.js
@@ -0,0 +1,22 @@
+export class QAData{
+    constructor() {
+        this.QAList =[
+            {
+                Q:"q1",
+                A:"a1"
+            },
+            {
+                Q:"q2",
+                A:"a2"
+            },
+            {
+                Q:"q3",
+                A:"a3"
+            },
+            {
+                Q:"q4",
+                A:"a4"
+            },
+        ]
+    }
+}
