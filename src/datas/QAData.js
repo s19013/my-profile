88ee0542480e0cc333f9be099f87df245fa86a7e
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-22 00:46:06 +0900

    データの書き方変更

--- src/datas/QAData.js
+++ src/datas/QAData.js
@@ -1,13 +1,39 @@
 export class QAData{
     constructor() {
-        this.QAList =[
+        this.list =[
             {
-                Q:"q1",
-                A:"a1"
+                Q:"興味がある､今後触ってみたい技術などは?",
+                A: `
+* GraphQL
+* fireBase
+* FastAPi
+* Flutter
+* 機械学習
+* ruby
+* ruby on rails
+* django
+* golang
+* PyScript
+* 低レイヤー
+* C言語
+* GCP
+* セキュリティ
+* 電気工作
+                `
             },
             {
-                Q:"q2",
-                A:"a2"
+                Q:"今後作ってみたいもの,技術的にやってみたいことは?",
+                A:`
+* laravelとvueを組み合わせたwebサービス
+* GraphQLを使ったwebサービス
+* 自作api
+* ラインチャットボット
+* サーバーから情報を取ってくるスマホアプリ
+* Flutterを使ったスマホアプリ
+* 自作ゲームに機械学習をしたコンピューターを搭載したい
+* 自分用のブックマーク
+* RasberyPyを使った電気工作
+                `
             },
             {
                 Q:"q3",
