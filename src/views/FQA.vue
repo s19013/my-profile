dbd8de317eca732be1ffa0cdecd3f840707d6a1b
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-22 00:45:47 +0900

    QAComponent設置

--- src/views/FQA.vue
+++ src/views/FQA.vue
@@ -1,15 +1,22 @@
 <template>
-  <div >
-    
+  <div>
+    <div v-for="(element,index) of QA.list" :key="index">
+      <q-a-component :data= element></q-a-component>
+    </div>
   </div>
 </template>
 
 <script>
+import { QAData } from '@/datas/QAData'
+import QAComponent from '@/components/QAComponent.vue'
   export default {
     name: 'FQAView',
+    components:{
+      QAComponent
+    },
     data() {
       return {
-        
+        QA:new QAData
       }
     },
   }
