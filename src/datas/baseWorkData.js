1762425d22fb26517f5531d9be3936fb9690ab7e
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-23 00:40:14 +0900

    色々やってみてる

--- src/datas/baseWorkData.js
+++ src/datas/baseWorkData.js
@@ -4,15 +4,50 @@ export class baseWorkData{
         title   = null,
         codeUrl = null,
         workUrl = null,
-        readmeUrl = null,
         comment = null,
+        readmeUrl = null,
         )
     {
         this.imgUrl  = imgUrl
         this.title   = title
         this.codeUrl = codeUrl
         this.workUrl = workUrl
-        this.readmeUrl = readmeUrl
         this.comment = comment
+        this.readmeUrl = readmeUrl
+    }
+
+    returnObject(){
+        return [
+            this.imgUrl,
+            this.title,
+            this.codeUrl,
+            this.workUrl,
+            this.comment,
+            this.readmeUrl,
+        ]
     }
 }
+// constructor()
+//     {
+//         this.imgUrl  = null
+//         this.title   = null
+//         this.codeUrl = null
+//         this.workUrl = null
+//         this.readmeUrl = null
+//         this.comment = null
+//     }
+//     cons({
+//         imgUrl  = null,
+//         title   = null,
+//         codeUrl = null,
+//         workUrl = null,
+//         readmeUrl = null,
+//         comment = null
+//     }) {
+//         this.imgUrl  = imgUrl
+//         this.title   = title
+//         this.codeUrl = codeUrl
+//         this.workUrl = workUrl
+//         this.readmeUrl = readmeUrl
+//         this.comment = comment
+//     }
