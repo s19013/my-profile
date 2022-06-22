e226acc1d1c71015afa4cfcc369a6f757ae8b0ab
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-22 18:11:09 +0900

    仮完成

---
+++ src/components/QAComponent.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+        <div @click="show = !show">
+            <div class="question">
+                <div class="markQ">Q</div>
+                <p>{{data.Q}}</p>
+                <v-btn icon class="btn">
+                    <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                </v-btn>
+            </div>
+        </div>
+        
+        <v-expand-transition>
+          <div v-show="show" class="answer">
+            <div class="markA">A</div>
+            <div v-html="markdownText"></div>
+          </div>
+        </v-expand-transition>
+  </div>
+</template>
+
+<script>
+import { marked } from 'marked'
+  export default {
+    name: 'QAComponent',
+    data() {
+        return {
+            show:false,
+            markdownText:marked.parse(this.data.A)
+        }
+    },
+    props:['data'],
+
+  }
+</script>
+
+<style lang="scss">
+.question{
+    display: flex;
+    margin: 10px 0;
+    .btn{
+        height: 40%;
+    //   margin:auto
+    //  display: flex;
+    //  justify-content: flex-end;
+    }
+    .markQ{
+        padding:1px 5px;
+        background-color: rgb(0, 191, 255);
+        font-weight: bold;
+        color: aliceblue;
+    }
+    p{
+        padding: 0 0 0 10px;
+    }
+}
+.answer{
+    // margin: 0 0 0 10px;
+    p{
+        margin: 0 0 0 30px;
+    }
+    li{
+        margin: 0 0 0 40px;
+    }
+    .markA{
+        width: 22px;
+        padding:1px 5px;
+        background-color: rgb(255, 72, 0);
+        font-weight: bold;
+        color: aliceblue;
+    }
+  }
+  
+</style>
