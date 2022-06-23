2cd59a5ef8f65837679f0183216a4cf7615a3c40
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-23 11:09:17 +0900

    new クラス名にすればできた

--- src/datas/MyWorksSampleWorks.js
+++ src/datas/MyWorksSampleWorks.js
@@ -1,24 +1,25 @@
+import { baseWorkData } from "./baseWorkData";
 export class MyWorksSampleWorks{
     constructor() {
         this.works = [
-            {
-                imgUrl:null,
-                title:"vue-todo",
-                readmeUrl:null,
-                codeUrl:"https://github.com/s19013/vue-todo2",
-                workUrl:"https://s19013.github.io/vue-todo2/",
-                comment:"保存機能はついていません",
-            },
-            {
-                imgUrl:null,
-                title:"vue-carousel-sample",
-                readmeUrl:null,
-                codeUrl:null,
-                workUrl:"https://s19013.github.io/vue-carousel-sample/",
-                comment:`
+            new baseWorkData(
+                {
+                    title:"vue-todo",
+                    codeUrl:"https://github.com/s19013/vue-todo2",
+                    workUrl:"https://s19013.github.io/vue-todo2/",
+                    comment:"保存機能はついていません",
+                }
+            ),
+            new baseWorkData(
+                {
+                    title:"vue-carousel-sample",
+                    workUrl:"https://s19013.github.io/vue-carousel-sample/",
+                    comment:`
 スライドショー
-                `,
-            },
+                    `,
+                }
+            ),
         ]
     }
+
 }
