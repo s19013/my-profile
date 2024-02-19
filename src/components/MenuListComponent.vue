cbd879fbc18dc4dd49fd826c1c44b4c4de820d01
知念英弥 <s19013@std.it-college.ac.jp>
2024-02-19 21:41:35 +0900

    font-sizeをcssだけで設定,remで

--- src/components/MenuListComponent.vue
+++ src/components/MenuListComponent.vue
@@ -1,50 +1,40 @@
 <template>
   <div class="menu">
-    <div v-for="(element,index) of list" :key="index">
-      <router-link :to="element.to"
-      class="router-link"
-      :style="size"
-      >
-        {{element.title}}
-        <font-awesome-icon :icon="element.icon" v-if="element.icon"/>
+    <div v-for="(element, index) of list" :key="index">
+      <router-link :to="element.to" class="router-link">
+        {{ element.title }}
+        <font-awesome-icon :icon="element.icon" v-if="element.icon" />
       </router-link>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'MenuListComponent',
-    data() {
-      return {
-        
-      }
-    },
-    props:['list','fontSize'],
-    computed:{
-      size(){
-        return {'--font-size':this.fontSize}
-      }
-    }
-  }
+export default {
+  name: "MenuListComponent",
+  data() {
+    return {};
+  },
+  props: ["list"],
+};
 </script>
 
 <style lang="scss" scoped>
-.menu{
+.menu {
   margin: auto;
   display: flex;
 }
 .router-link {
-    margin:0 2vmin;
-    font-size:var(--font-size);
+  margin: 0 2vmin;
+  font-size: 1.5rem;
 }
-a{
+a {
   padding: 0 1vw;
   color: black;
   text-decoration: none;
 }
-.router-link-active{
-  background-color:#112d4e;
+.router-link-active {
+  background-color: #112d4e;
   color: #f9f7f7;
   font-weight: bold;
 }
