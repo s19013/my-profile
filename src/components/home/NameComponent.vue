5df3f0d3e7710728ce92c5ec008b233ed9c58bf8
知念英弥 <s19013@std.it-college.ac.jp>
2024-02-19 21:57:46 +0900

    フォーマッターかけた remで書き直し

--- src/components/home/NameComponent.vue
+++ src/components/home/NameComponent.vue
@@ -1,51 +1,47 @@
 <template>
-
-<div>
-  <div class="myName">
-    <div class="familyName">
-      <h5>{{name.familyNameRubi}}</h5>
-      <h2>{{name.familyName    }}</h2>
-    </div>
-    <div class="secondName">
-      <h5>{{name.secondNameRubi}}</h5>
-      <h2>{{name.secondName    }}</h2>
+  <div>
+    <div class="myName">
+      <div class="familyName">
+        <h5>{{ name.familyNameRubi }}</h5>
+        <h2>{{ name.familyName }}</h2>
+      </div>
+      <div class="secondName">
+        <h5>{{ name.secondNameRubi }}</h5>
+        <h2>{{ name.secondName }}</h2>
+      </div>
     </div>
   </div>
-</div>
-
 </template>
 
 <script>
-  export default {
-    name: 'NameComponent',
-    data() {
-      return {
-        
-      }
-    },
-    props:['name']
-  }
+export default {
+  name: "NameComponent",
+  data() {
+    return {};
+  },
+  props: ["name"],
+};
 </script>
 
 <style lang="scss" scoped>
-  .myName{
-    display: grid;
-    grid-template-columns: 50% 50%;
-    grid-template-rows: 50% 50%;
-    gap: 1rem;
-    .familyName{
-      text-align: center;
-      margin-left: auto;
-    }
-    .secondName{
-      text-align: center;
-      margin-right: auto;
-    }
-    h2{
-      font-size:200%
-    }
-    h5{
-      font-size:80%
-    }
+.myName {
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: 50% 50%;
+  gap: 1rem;
+  .familyName {
+    text-align: center;
+    margin-left: auto;
   }
+  .secondName {
+    text-align: center;
+    margin-right: auto;
+  }
+  h2 {
+    font-size: 4rem;
+  }
+  h5 {
+    font-size: 1rem;
+  }
+}
 </style>
