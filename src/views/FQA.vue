3c8cd9b59ce7fe267991bf0df94fbae402f9a2ed
知念英弥 <s19013@std.it-college.ac.jp>
2024-02-19 23:07:33 +0900

    フォーマッターかけた

--- src/views/FQA.vue
+++ src/views/FQA.vue
@@ -2,72 +2,78 @@
   <div>
     <v-row>
       <v-btn-toggle
-          color="deep-purple accent-3"
-          class="tags"
-          v-model="toggle_exclusive"
-          mandatory
+        color="deep-purple accent-3"
+        class="tags"
+        v-model="toggle_exclusive"
+        mandatory
+      >
+        <v-btn value="すべて" class="v-btn" @click="filter(null)">すべて</v-btn>
+        <v-btn value="技術" class="v-btn" @click="filter('技術')">技術</v-btn>
+        <v-btn value="興味" class="v-btn" @click="filter('興味')"
+          >興味/関心</v-btn
         >
-        
-          <v-btn value="すべて" class="v-btn" @click="filter(null)"    >すべて</v-btn>
-          <v-btn value="技術"   class="v-btn" @click="filter('技術')"  >技術</v-btn>
-          <v-btn value="興味"   class="v-btn" @click="filter('興味')"  >興味/関心</v-btn>
-          <v-btn value="性格"   class="v-btn" @click="filter('性格')"  >性格</v-btn>
-          <v-btn value="考え"   class="v-btn" @click="filter('考え')"  >考え</v-btn>
-          <v-btn value="過去"   class="v-btn" @click="filter('過去')"  >過去</v-btn>
-          <v-btn value="その他" class="v-btn" @click="filter('その他')"  >その他</v-btn>
+        <v-btn value="性格" class="v-btn" @click="filter('性格')">性格</v-btn>
+        <v-btn value="考え" class="v-btn" @click="filter('考え')">考え</v-btn>
+        <v-btn value="過去" class="v-btn" @click="filter('過去')">過去</v-btn>
+        <v-btn value="その他" class="v-btn" @click="filter('その他')"
+          >その他</v-btn
+        >
       </v-btn-toggle>
     </v-row>
 
-    <div v-for="(element) of QAList" :key="element.Q">
-      <q-a-component :data= element> </q-a-component>
+    <div v-for="element of QAList" :key="element.Q">
+      <q-a-component :data="element"> </q-a-component>
     </div>
-
   </div>
 </template>
 
 <script>
-import { QAData } from '@/datas/QAData'
-import QAComponent from '@/components/QAComponent.vue'
-  export default {
-    name: 'FQAView',
-    components:{
-      QAComponent
-    },
-    data() {
-      return {
-        QA:new QAData,
-        QAList:null,
-        //トグル管理
-        toggle_exclusive: undefined,
+import { QAData } from "@/datas/QAData";
+import QAComponent from "@/components/QAComponent.vue";
+export default {
+  name: "FQAView",
+  components: {
+    QAComponent,
+  },
+  data() {
+    return {
+      QA: new QAData(),
+      QAList: null,
+      //トグル管理
+      toggle_exclusive: undefined,
+    };
+  },
+  methods: {
+    filter(arg = null) {
+      // 引数が空の時
+      // すべて表示する
+      if (arg == null) {
+        return (this.QAList = this.QA.list);
       }
-    },
-    methods:{
-      filter(arg = null){
-        // 引数が空の時
-        // すべて表示する
-        if (arg == null) {return  this.QAList = this.QA.list}
 
-        //引数でフィルタリングする
-        const List = []
-        for(const element of this.QA.list){
-          if(element.tag.includes(arg)){ List.push(element) }
+      //引数でフィルタリングする
+      const List = [];
+      for (const element of this.QA.list) {
+        if (element.tag.includes(arg)) {
+          List.push(element);
         }
-          return this.QAList = List
       }
+      return (this.QAList = List);
     },
-    mounted() {
-      this.filter(null)
-    },
-  }
+  },
+  mounted() {
+    this.filter(null);
+  },
+};
 </script>
 
-<style lang="scss" >
+<style lang="scss">
 @media (min-width: 451px) {
-  .tags{
-  flex-wrap:wrap;
-  margin: 10px auto;
-    button{
-      width:12vw;
+  .tags {
+    flex-wrap: wrap;
+    margin: 10px auto;
+    button {
+      width: 12vw;
     }
     /* p{
       width:2vw;
@@ -75,11 +81,11 @@ import QAComponent from '@/components/QAComponent.vue'
   }
 }
 @media (max-width: 450px) {
-  .tags{
-  flex-wrap:wrap;
-  margin: 20px auto;
-    button{
-      width:25vw;
+  .tags {
+    flex-wrap: wrap;
+    margin: 20px auto;
+    button {
+      width: 25vw;
     }
     /* p{
       width:2vw;
