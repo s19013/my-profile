24d2d864292fb6de3ee33bd9eaa79faf5fcf4aae
知念英弥 <s19013@std.it-college.ac.jp>
2024-02-21 22:28:41 +0900

    まずはoriginalだけを表示

--- src/views/MyWorks/MyWorks.vue
+++ src/views/MyWorks/MyWorks.vue
@@ -1,51 +1,55 @@
 <template>
   <div>
-    <menu-list-component
-      class="menuComponent"
-      :list="menuList.MyWorksList"
-      :fontSize="menuList.MyWorksListFontSize"
-    ></menu-list-component>
-    <router-view></router-view>
+    <div class="heading">
+      <p>{{ works.pageTitle }}</p>
+    </div>
+    <div class="cardGroup">
+      <!-- vue2なのでこの書き方 -->
+      <template v-for="(element, index) of works.works">
+        <work-card-component :work="element" :key="index"></work-card-component>
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
-import MenuListComponent from "@/components/MenuListComponent.vue";
-
-// Appでも呼び出して､ここでも呼び出すって無駄?props使うべき?
-import { MenuListData } from "@/datas/MenuListData";
+import WorkCardComponent from "@/components/WorkCardComponent.vue";
+import { MyWorksOriginalWorks } from "@/datas/MyWorksOriginalWorks.js";
 export default {
   name: "MyWorks",
-  components: {
-    MenuListComponent,
-  },
+  components: { WorkCardComponent },
   data() {
     return {
-      menuList: new MenuListData(),
+      // 初期値はoriginal
+      // works.worksとなってややこしいためもっと適切な変数を探す｡
+      works: new MyWorksOriginalWorks(),
     };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-@media (min-width: 951px) {
-  .workCard {
-    width: 25vw;
-    margin: 1rem;
-  }
-}
-
-@media (min-width: 451px) and (max-width: 950px) {
-  .workCard {
-    width: 42vw;
-    margin: 2vh 2vw;
-  }
-}
-
-@media (max-width: 450px) {
-  .workCard {
+.workCard {
+  width: 49%;
+  @media (max-width: 450px) {
     width: 100%;
-    margin: 5vh 0;
+  }
+}
+
+.cardGroup {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  @media (max-width: 450px) {
+    gap: 2rem;
+  }
+}
+
+.heading {
+  border-bottom: 1px solid black;
+  margin: 1rem 0;
+  p {
+    margin: 1rem 0;
   }
 }
 .menuComponent {
