bd5de41c3410d0b490fff088299c3f0f0ba8b9e1
知念英弥 <s19013@std.it-college.ac.jp>
2024-02-22 20:38:05 +0900

    Articleコンポーネントと連結

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>article</div>
+  <div>
+    <template v-for="article of articleData.articles">
+      <ArticleComponent
+        :article="article"
+        :key="article.url"
+      ></ArticleComponent>
+    </template>
+  </div>
 </template>
 
 <script>
+import ArticleComponent from "@/components/ArticleComponent.vue";
+import ArticleData from "@/datas/ArticleData";
 export default {
   name: "ArticleView",
+  components: {
+    ArticleComponent,
+  },
+  data() {
+    return {
+      articleData: new ArticleData(),
+    };
+  },
 };
 </script>
 
