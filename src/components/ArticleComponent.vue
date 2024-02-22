397f36ee83b3afc2130eb53defe05a4d70621521
知念英弥 <s19013@std.it-college.ac.jp>
2024-02-22 23:08:37 +0900

    アイコンや日付を追加｡ 仮の文字列をおいてcssを確認

--- src/components/ArticleComponent.vue
+++ src/components/ArticleComponent.vue
@@ -8,6 +8,13 @@
         <p :key="index">{{ row }}</p>
       </template>
     </div>
+    <p class="created_at">created_at:{{ date() }}</p>
+    <div class="counts">
+      <p><font-awesome-icon icon="fa-regular fa-comment" />111</p>
+      <p><font-awesome-icon icon="fa-regular fa-heart" />111</p>
+      <p><font-awesome-icon icon="fa-brands fa-get-pocket" />111</p>
+      <p><font-awesome-icon icon="fa-regular fa-eye" />111</p>
+    </div>
   </div>
 </template>
 
@@ -19,7 +26,30 @@ export default {
       required: true,
     },
   },
+  methods: {
+    date(arg = "2000-01-01T00:00:00+00:00") {
+      // Tより前の文字列たちを返す
+      return arg.match(/^.*(?=T)/)[0];
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.article {
+  border: 1px solid black;
+  padding: 1rem;
+}
+.comment {
+  margin-top: 0.5rem;
+}
+.created_at {
+  text-align: end;
+}
+.counts {
+  display: flex;
+  justify-content: flex-end;
+  gap: 1rem;
+  flex-wrap: wrap;
+}
+</style>
