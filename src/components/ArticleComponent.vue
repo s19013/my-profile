ad7d68ced827c9da5119680bcfbbe2053d87d5f7
知念英弥 <s19013@std.it-college.ac.jp>
2024-02-22 20:37:13 +0900

    まずは表示するだけ

---
+++ src/components/ArticleComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="article">
+    <a :href="article.url">
+      <h3 class="Title">{{ article.title }}</h3>
+    </a>
+    <div class="comment">
+      <template v-for="(row, index) of article.comment">
+        <p :key="index">{{ row }}</p>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
