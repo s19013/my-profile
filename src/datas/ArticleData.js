18ba3cdd698b753e25635700b6b6925fbad260bb
知念英弥 <s19013@std.it-college.ac.jp>
2024-02-23 17:23:34 +0900

    qiita,apiを使うためidを持つようにした

--- src/datas/ArticleData.js
+++ src/datas/ArticleData.js
@@ -1,17 +1,11 @@
 export default class ArticleData {
     articles = [
         {
-            title:"エンジニアになれないへっぽこな私が実装したタグをつける機能とタグの検索機能(n番煎じ)",
-            url:"https://qiita.com/hideya670/items/268a9643488e4036706a",
+            id:"268a9643488e4036706a",
             comment:[
                 "advent calendarというイベントに参加して書いた記事です｡",
                 "自分のアプリで実装したタグ機能とタグを検索する機能がどんな風になっているのかを書いています｡"
             ]
         },
-        // {
-        //     title:null,
-        //     url:null,
-        //     comment:[null]
-        // },
     ]
 }
