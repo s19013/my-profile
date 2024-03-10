33effcfa5c28e2fcfdeefc6d43cd32bd93ea1897
知念英弥 <s19013@std.it-college.ac.jp>
2024-03-10 23:39:05 +0900

    スキルをまとめたview

---
+++ src/views/Skill.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="skills">
+    <template v-for="skill of skills">
+      <skill-component :skill="skill" :key="skill.name"></skill-component>
+    </template>
+  </div>
+</template>
+
+<script>
+import SkillComponent from "../components/SkillComponent.vue";
+
+import { SkillData } from "../datas/SkillData.js";
+export default {
+  name: "SkillView",
+  components: {
+    SkillComponent,
+  },
+  data() {
+    return {
+      skills: new SkillData().skills,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
