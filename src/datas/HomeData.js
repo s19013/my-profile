73e5ad8d5204982231a5006487699dff75946e7f
知念英弥 <s19013@std.it-college.ac.jp>
2024-03-10 15:36:02 +0900

    プライバシー保護のために本名と､学歴などを表示しないようにした｡

--- src/datas/HomeData.js
+++ src/datas/HomeData.js
@@ -1,12 +1,7 @@
 export class HomeData{
     constructor(){
         // 名前
-        this.name = {
-            familyName:"知念",
-            familyNameRubi:"ちねん",
-            secondName:"英弥",
-            secondNameRubi:"ひでや"
-        }
+        this.nickName = "ヒデ"
         // 生年月日
         this.birthDay = {
             year :1999,
@@ -29,34 +24,6 @@ export class HomeData{
                 icon :null,
                 title:'connpass',
                 url  :'https://connpass.com/user/hideya0522/',
-            },
-        ]
-        // 職歴,学歴
-        this.careerList = [
-            {
-                year  :2015,
-                month :3,
-                detail:"那覇市立小禄中学校 卒業"
-            },
-            {
-                year  :2015,
-                month :4,
-                detail:"沖縄県立那覇高等学校 入学"
-            },
-            {
-                year  :2018,
-                month :3,
-                detail:"沖縄県立那覇高等学校 卒業"
-            },
-            {
-                year  :2019,
-                month :4,
-                detail:"専門学校ITカレッジ沖縄 ITスペシャリスト科 入学"
-            },
-            {
-                year  :2022,
-                month :3,
-                detail:"専門学校ITカレッジ沖縄 ITスペシャリスト科 卒業"
             },
         ]
         this.qualificationList = [
